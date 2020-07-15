f8818f84cd11b8610ae4a1230b3f768f20ca4597
Mateusz Markowicz <poniekad@protonmail.com>
2020-07-15 19:10:52 +0200

    Add testing for certificate pinning

--- network/data/src/test/java/me/proton/core/network/data/util/TestUtils.kt
+++ network/data/src/test/java/me/proton/core/network/data/util/TestUtils.kt
@@ -17,12 +17,66 @@
  */
 package me.proton.core.network.data.util
 
+import me.proton.core.network.data.initPinning
+import me.proton.core.network.data.initSPKIleafPinning
+import okhttp3.OkHttpClient
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
+import java.io.File
+import java.security.KeyStore
+import javax.net.ssl.KeyManagerFactory
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.TrustManagerFactory
+import javax.net.ssl.X509TrustManager
 
 fun MockWebServer.prepareResponse(code: Int, body: String = "") {
     val response = MockResponse()
         .setResponseCode(code)
         .setBody(body)
     enqueue(response)
+}
+
+class TestTLSHelper {
+
+    lateinit var trustManagers: Array<TrustManager>
+    lateinit var sslContext: SSLContext
+
+    init {
+        File("./src/test/resources/test.jks").inputStream().use { jksInput ->
+            val pass = "test00".toCharArray()
+            val serverKeyStore: KeyStore = KeyStore.getInstance(KeyStore.getDefaultType())
+            serverKeyStore.load(jksInput, pass)
+
+            val algorithm = KeyManagerFactory.getDefaultAlgorithm()
+            val keyManagerFactory = KeyManagerFactory.getInstance(algorithm)
+            keyManagerFactory.init(serverKeyStore, pass)
+
+            val trustManagerFactory = TrustManagerFactory.getInstance(algorithm)
+            trustManagerFactory.init(serverKeyStore)
+
+            trustManagers = trustManagerFactory.trustManagers
+            sslContext = SSLContext.getInstance("TLS")
+            sslContext.init(keyManagerFactory.keyManagers, trustManagers, null)
+        }
+    }
+
+    fun createMockServer() = MockWebServer().apply {
+        useHttps(sslContext.socketFactory, false)
+    }
+
+    fun initPinning(builder: OkHttpClient.Builder, pins: Array<String>) {
+        builder.sslSocketFactory(sslContext.socketFactory, trustManagers.first() as X509TrustManager)
+        initPinning(builder, "localhost", pins)
+    }
+
+    fun setupSPKIleafPinning(builder: OkHttpClient.Builder, pins: List<String>) {
+        builder.sslSocketFactory(sslContext.socketFactory, trustManagers.first() as X509TrustManager)
+        initSPKIleafPinning(builder, pins)
+    }
+
+    companion object {
+        val TEST_PINS = arrayOf("sha256/d/dc7p/QKB+PnyVi/JOHUQxrZpfGc2LEdq43JGGii4k=")
+        val BAD_PINS  = arrayOf("sha256/a/dc7p/QKB+PnyVi/JOHUQxrZpfGc2LEdq43JGGii4k=")
+    }
 }
