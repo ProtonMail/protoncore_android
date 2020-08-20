d6f5a47e728cc867c568819cdb0cabfc6eecdce8
Davide Farella <4face91@protonmail.com>
2020-08-20 09:48:31 +0200

    Util Gradle 0.1.10: Bump Detekt to 1.11.2
    CP-636
    
    fixup! Util Gradle 0.1.10: Bump Detekt to 1.11.2
    
    Wildcard import

--- util/gradle/src/main/kotlin/me/proton/core/util/gradle/detekt.kt
+++ util/gradle/src/main/kotlin/me/proton/core/util/gradle/detekt.kt
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2020 Proton Technologies AG
+ * This file is part of Proton Technologies AG and ProtonCore.
+ *
+ * ProtonCore is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonCore is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonCore.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 @file:Suppress("unused")
 
 package me.proton.core.util.gradle
@@ -37,7 +55,7 @@ import java.util.Properties
  */
 fun Project.setupDetekt(filter: (Project) -> Boolean = { true }) {
 
-    `detekt version` = "1.10.0" // Released: Jul 07, 2020
+    `detekt version` = "1.11.2" // Released: Aug 19, 2020
     `detect-code-analysis version` = "0.3.2" // Released:
 
     val reportsDirPath = "config/detekt/reports"
