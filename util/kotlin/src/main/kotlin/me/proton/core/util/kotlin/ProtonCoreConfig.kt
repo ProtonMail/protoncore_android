f0d0645d33bd020d7fd4a696b3737592d53cf4d0
Davide Farella <4face91@protonmail.com>
2020-08-28 13:51:00 +0200

    Fix ProtonCoreConfig.kt code style

--- util/kotlin/src/main/kotlin/me/proton/core/util/kotlin/ProtonCoreConfig.kt
+++ util/kotlin/src/main/kotlin/me/proton/core/util/kotlin/ProtonCoreConfig.kt
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2020 Proton Technologies AG
+ * This file is part of Proton Technologies AG and ProtonCore.
+ *
+ * ProtonCore is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonCore is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonCore.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package me.proton.core.util.kotlin
 
 import kotlinx.serialization.StringFormat
@@ -11,6 +29,9 @@ import kotlinx.serialization.json.JsonConfiguration
 object ProtonCoreConfig : Invokable {
 
     /** Default [StringFormat] for serialize and deserialize JSON strings */
-    var defaultJsonStringFormat = Json(JsonConfiguration.Stable.copy(
-        ignoreUnknownKeys = true, isLenient = true))
+    var defaultJsonStringFormat = Json(
+        JsonConfiguration.Stable.copy(
+            ignoreUnknownKeys = true, isLenient = true
+        )
+    )
 }
