9b24798ad909c08dc68853bac9959286596c4a98
Davide Farella <4face91@protonmail.com>
2020-10-14 12:37:09 +0200

    Create ProtonCorePlugin.kt
    CP-964

---
+++ gradle/plugin/build.gradle.kts
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2020 Proton Technologies AG
+ * This file is part of Proton Technologies AG and ProtonCore.
+ *
+ * ProtonCore is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonCore is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonCore.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+plugins {
+    `kotlin-dsl`
+    kotlin("jvm") version "1.4.10"
+    id("java-gradle-plugin")
+}
+
+group = "me.proton"
+version = "1.0"
+
+gradlePlugin {
+    plugins {
+        create("gradlePlugin") {
+            id = "core"
+            implementationClass = "ProtonCorePlugin"
+        }
+    }
+}
+
+repositories {
+    google()
+    jcenter()
+}
+
+dependencies {
+    val easyGradle = "2.1" // Aug 08, 2020
+    val agpVersion = "4.2.0-alpha13" // Oct 01, 2020
+
+    implementation(gradleApi())
+    api("studio.forface.easygradle:dsl:$easyGradle")
+    compileOnly("com.android.tools.build:gradle:$agpVersion")
+}
