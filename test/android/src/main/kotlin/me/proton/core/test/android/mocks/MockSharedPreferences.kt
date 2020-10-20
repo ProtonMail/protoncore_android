930a454c4de83dbda9d467e5783c634b7efc008e
Davide Farella <4face91@protonmail.com>
2020-10-20 18:34:07 +0200

    Add MockSharedPreferences.kt mock call for getFloat and getStringSet
    android-test [0.2] -> [0.2.1]

--- test/android/src/main/kotlin/me/proton/core/test/android/mocks/MockSharedPreferences.kt
+++ test/android/src/main/kotlin/me/proton/core/test/android/mocks/MockSharedPreferences.kt
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2020 Proton Technologies AG
+ * This file is part of Proton Technologies AG and ProtonCore.
+ *
+ * ProtonCore is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonCore is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonCore.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package me.proton.core.test.android.mocks
 
 import android.content.SharedPreferences
@@ -28,9 +46,11 @@ val newMockSharedPreferences
 
         every { all } answers { m }
         every { getBoolean(any(), any()) } answers { getMap() }
+        every { getFloat(any(), any()) } answers { getMap() }
         every { getInt(any(), any()) } answers { getMap() }
         every { getLong(any(), any()) } answers { getMap() }
         every { getString(any(), any()) } answers { getMap() }
+        every { getStringSet(any(), any()) } answers { getMap() }
     }
 
 private class MockSharedPreferencesEditor(
@@ -38,9 +58,9 @@ private class MockSharedPreferencesEditor(
 ) : SharedPreferences.Editor {
 
     override fun putBoolean(key: String, value: Boolean) = apply { m[key] = value }
+    override fun putFloat(key: String, value: Float) = apply { m[key] = value }
     override fun putInt(key: String, value: Int) = apply { m[key] = value }
     override fun putLong(key: String, value: Long) = apply { m[key] = value }
-    override fun putFloat(key: String, value: Float) = apply { m[key] = value }
     override fun putString(key: String, value: String?) = apply { m[key] = value }
     override fun putStringSet(key: String, values: MutableSet<String>?) = apply { m[key] = values }
     override fun remove(key: String) = apply { m -= key }
