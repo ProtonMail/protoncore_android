f0b945c57b7920e42487b30918c9a8abea23cc31
Davide Farella <4face91@protonmail.com>
2020-12-18 15:50:18 +0100

    Reduce Android minSdk [23] -> [21]

--- gradle/plugin/src/main/kotlin/ProtonCore.kt
+++ gradle/plugin/src/main/kotlin/ProtonCore.kt
@@ -27,7 +27,7 @@ object ProtonCore {
     /** The Android API level as target of the App */
     const val targetSdk = 30
     /** The Android API level required for run the App */
-    const val minSdk = 23
+    const val minSdk = 21
     /** The version of the JDK  */
     val jdkVersion = JavaVersion.VERSION_1_8
 }
