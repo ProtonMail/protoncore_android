2967d9bff0195d6617006beb66843a7000f7ddca
dkadrikj <kadrikj@protonmail.ch>
2020-12-29 12:33:48 +0100

    CP-1280

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -23,7 +23,7 @@ plugins {
     kotlin("android")
 }
 
-libVersion = Version(0, 2, 8)
+libVersion = Version(0, 2, 9)
 
 android()
 
--- auth/data/build.gradle.kts
+++ auth/data/build.gradle.kts
@@ -24,7 +24,7 @@ plugins {
     kotlin("plugin.serialization")
 }
 
-libVersion = Version(0, 2, 8)
+libVersion = Version(0, 2, 9)
 
 android()
 
