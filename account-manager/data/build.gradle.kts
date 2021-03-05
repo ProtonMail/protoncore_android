0a1d829b12dc645ac1249e7e1384cb2605650a9a
Neil Marietta <neil.marietta@protonmail.com>
2021-03-05 11:28:41 +0100

    Bump modules version to 1.0.2 (fix rename "simple" to "keystore").

--- account-manager/data/build.gradle.kts
+++ account-manager/data/build.gradle.kts
@@ -25,7 +25,7 @@ plugins {
     kotlin("kapt")
 }
 
-libVersion = Version(1, 0, 1)
+libVersion = Version(1, 0, 2)
 
 android(minSdk = 23) {
     defaultConfig {
--- account-manager/presentation/build.gradle.kts
+++ account-manager/presentation/build.gradle.kts
@@ -25,7 +25,7 @@ plugins {
     kotlin("android.extensions")
 }
 
-libVersion = Version(1, 0, 1)
+libVersion = Version(1, 0, 2)
 
 android()
 
