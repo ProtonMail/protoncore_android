bc8329c10df78cd9f37a352a85ac5fed1f684c11
Neil Marietta <neil.marietta@protonmail.com>
2021-06-17 17:03:17 +0200

    Added Account.isStepNeeded.

--- account/build.gradle.kts
+++ account/build.gradle.kts
@@ -24,7 +24,7 @@ plugins {
     kotlin("android")
 }
 
-libVersion = Version(1, 1, 0)
+libVersion = Version(1, 1, 7)
 
 android()
 
@@ -35,4 +35,3 @@ dependencies {
         project(Module.accountData)
     )
 }
-
