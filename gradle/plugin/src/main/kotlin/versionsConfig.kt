77cf6f0771a7546587f74d5ffe72f09f640259c7
Vincent Brison <vincent.brison@proton.ch>
2021-09-06 17:36:27 +0200

    Update core libraries dependencies

--- gradle/plugin/src/main/kotlin/versionsConfig.kt
+++ gradle/plugin/src/main/kotlin/versionsConfig.kt
@@ -50,10 +50,10 @@ internal fun initVersions() {
     `dagger version` =                          "2.38.1"        // Released: Jul 27, 2021
     `hilt-android version` =                    "2.38.1"        // Released: Jul 27, 2021
     `hilt-androidx version` =                   "1.0.0"         // Released: May 05, 2021
-    `mockK version` =                           "1.10.5"        // Released: Jan 13, 2021
+    `mockK version` =                           "1.12.0"
     `retrofit version` =                        "2.9.0"         // Released: May 20, 2020
     `retrofit-kotlin-serialization version` =   "0.8.0"         // Released: Oct 09, 2020
-    `timber version` =                          "4.7.1"         // Released: Jun 28, 2018
+    `timber version` =                          "5.0.1"         // Released: Jun 28, 2018
     `viewStateStore version` =                  "1.4-beta-4"    // Released: Oct 03, 2019
     // endregion
 }
@@ -64,25 +64,25 @@ const val `androidUi version` =             "0.1.0-dev08"   // Released: Apr 03,
 // endregion
 
 // region Other
-const val `apacheCommon-codec version` =    "1.14"          // Released: Dec 31, 2019
+const val `apacheCommon-codec version` =    "1.15"
 const val `bcrypt version` =                "0.9.0"         // Released: Oct 29, 2019
-const val `gotev-cookieStore version` =     "1.3.1"         // Released: Oct 24, 2020
-const val `googleTink version` =            "1.5.0"         // Released: Oct 15, 2020
+const val `gotev-cookieStore version` =     "1.3.5"
+const val `googleTink version` =            "1.6.1"         // Released: Oct 15, 2020
 const val `miniDsn version` =               "1.0.0"         // Released: Jul 18, 2020
-const val `okHttp version` =                "4.8.0"         // Released: Jul 11, 2020
-const val `okHttp-url-connection version` = "4.9.0"         // Released: Sep, 2020
-const val `trustKit version` =              "1.1.3"         // Released: Apr 30, 2020
+const val `okHttp version` =                "4.9.1"
+const val `okHttp-url-connection version` = "4.9.1"
+const val `trustKit version` =              "1.1.5"
 const val `store4 version` =                "4.0.2-KT15"    // Released: May 17, 2021
 const val `lifecycle-extensions version` =  "2.2.0"         // Released: Jan 00, 2020
-const val `lottie version` =                "3.7.2"         // Released: Jul 18, 2021
+const val `lottie version` =                "4.1.0"
 
 // endregion
 
 // region Tests
-const val `falcon version` =                    "2.1.1"         // Released: Sep 24, 2018
+const val `falcon version` =                    "2.2.0"         // Released: Sep 24, 2018
 const val `uiautomator version` =               "2.2.0"         // Released: Oct 25, 2018
 const val `preference version` =                "1.1.1"         // Released: Apr 15, 2020
 const val `json-simple version` =               "1.1.1"         // Released: Mar 21, 2012
-const val `turbine version` =                   "0.6.0"         // Released: Jul 27, 2021
-const val `junit-ktx version` =                 "1.1.2"         // Released: Aug 26, 2020
+const val `turbine version` =                   "0.6.1"
+const val `junit-ktx version` =                 "1.1.3"
 // endregion
