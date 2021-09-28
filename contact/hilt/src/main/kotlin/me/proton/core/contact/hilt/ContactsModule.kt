1d51f829cf6bd16093b74ff3fd915b0acc88df33
Vincent Brison <vincent.brison@proton.ch>
2021-09-28 18:56:13 +0200

    Add contact hilt module

--- contact/hilt/src/main/kotlin/me/proton/core/contact/hilt/ContactsModule.kt
+++ contact/hilt/src/main/kotlin/me/proton/core/contact/hilt/ContactsModule.kt
@@ -16,7 +16,7 @@
  * along with ProtonCore.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package me.proton.android.core.coreexample.di
+package me.proton.core.contact.hilt
 
 import dagger.Binds
 import dagger.Module
