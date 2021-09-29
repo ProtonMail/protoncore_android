881baf872d799413f3879c7311e9aac6f3bc6ab6
Vincent Brison <vincent.brison@proton.ch>
2021-09-29 18:01:36 +0200

    Bump version

--- contact/build.gradle.kts
+++ contact/build.gradle.kts
@@ -23,7 +23,7 @@ plugins {
     kotlin("android")
 }
 
-libVersion = Version(1, 16, channel = Version.Channel.Build, build = 3)
+libVersion = Version(1, 16, channel = Version.Channel.Build, build = 4)
 
 android()
 
