c078c13cc01f9ad0469ec6ae114358d16b75a9a9
Vincent Brison <vincent.brison@proton.ch>
2021-10-25 16:57:28 +0200

    Support no proxy token in UI test Without this change NPE is thrown if trying to run UI test without PROXY_TOKEN set.

--- test/android/instrumented/build.gradle.kts
+++ test/android/instrumented/build.gradle.kts
@@ -19,7 +19,7 @@
 import studio.forface.easygradle.dsl.*
 import studio.forface.easygradle.dsl.android.*
 
-libVersion = Version(1, 15, 1)
+libVersion = Version(1, 15, 2)
 
 plugins {
     id("com.android.library")
