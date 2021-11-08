dbf6e05e3bfdf98777e15a2bc187b50380c9cfb3
Vincent Brison <vincent.brison@proton.ch>
2021-11-08 17:42:07 +0100

    Prepare release for Test Android Instrumented

--- test/android/instrumented/build.gradle.kts
+++ test/android/instrumented/build.gradle.kts
@@ -19,7 +19,7 @@
 import studio.forface.easygradle.dsl.*
 import studio.forface.easygradle.dsl.android.*
 
-libVersion = Version(1, 15, 3)
+libVersion = Version(1, 15, 4)
 
 plugins {
     id("com.android.library")
