f4b7ecbb6757c7ca68f2cb5db433b5c71c8110bf
Mateusz Armatys <mateusz.armatys@proton.ch>
2021-11-12 17:53:20 +0100

    Add custom LogTag for auth-presentation module

---
+++ auth/presentation/src/main/kotlin/me/proton/core/auth/presentation/LogTag.kt
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2021 Proton Technologies AG
+ * This file is part of Proton Technologies AG and ProtonCore.
+ *
+ * ProtonCore is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonCore is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonCore.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package me.proton.core.auth.presentation
+
+object LogTag {
+    /** Tag for marking when a flow has failed with an exception, but it will be retried. */
+    const val FLOW_ERROR_RETRY = "core.auth.presentation.flow.retry"
+}
