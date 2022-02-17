fdcaa3aa0d36be12692e3ccf13728d2e4c5855eb
Mateusz Armatys <mateusz.armatys@proton.ch>
2022-02-17 18:40:54 +0100

    Redirect logs from root java.util.logging.Logger to CoreLogger

---
+++ util/kotlin/src/main/kotlin/me/proton/core/util/kotlin/LoggingHandler.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton AG and ProtonCore.
+ *
+ * ProtonCore is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonCore is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonCore.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package me.proton.core.util.kotlin
+
+import java.util.logging.Filter
+import java.util.logging.Handler
+import java.util.logging.Level
+import java.util.logging.LogRecord
+
+internal class LoggingHandler(
+    private val logger: Logger,
+    loggingFilter: Filter? = null
+) : Handler() {
+    init {
+        filter = loggingFilter
+    }
+
+    override fun close() {}
+    override fun flush() {}
+
+    override fun publish(record: LogRecord) {
+        if (!isLoggable(record)) return
+
+        val tag: String = record.loggerName ?: record.sourceName() ?: "LoggingHandler"
+        val logLevel = record.level.intValue()
+        val message: String = record.message ?: ""
+        val thrown = Throwable("LoggingHandler: $message", record.thrown)
+        when {
+            logLevel >= Level.SEVERE.intValue() -> logger.e(tag, thrown, message)
+            logLevel >= Level.CONFIG.intValue() -> logger.d(tag, thrown, message)
+            else -> logger.v(tag, thrown, message)
+        }
+    }
+
+    private fun LogRecord.sourceName(): String? {
+        val separator = ":"
+        return buildString {
+            if (sourceClassName != null) append(sourceClassName)
+            append(separator)
+            if (sourceMethodName != null) append(sourceMethodName)
+        }.takeIf { it.length > separator.length }
+    }
+}
