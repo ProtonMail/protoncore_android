0c94fc08550e01311acf2a159d71beb6602b1b31
dkadrikj <kadrikj@protonmail.ch>
2022-08-10 15:02:30 +0200

    feat (payment-iap): Add new payment GIAP module

---
+++ payment-iap/presentation/src/main/kotlin/me/proton/core/paymentiap/presentation/ui/BillingIAPFragment.kt
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton AG and ProtonCore.
+ *
+ * ProtonCore is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonCore is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonCore.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package me.proton.core.paymentiap.presentation.ui
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import me.proton.core.paymentcommon.presentation.entity.PlanShortDetails
+import me.proton.core.paymentcommon.presentation.viewmodel.BillingViewModel
+import me.proton.core.paymentiap.presentation.R
+import me.proton.core.paymentiap.presentation.databinding.FragmentBillingIapBinding
+import me.proton.core.presentation.ui.ProtonFragment
+import me.proton.core.presentation.utils.viewBinding
+import me.proton.core.util.kotlin.exhaustive
+
+/**
+ * Fragment that handles Billing In App Purchases from Google.
+ */
+@AndroidEntryPoint
+public class BillingIAPFragment : ProtonFragment(R.layout.fragment_billing_iap) {
+
+    private val viewModel: BillingViewModel by viewModels({ requireActivity() })
+    private val binding by viewBinding(FragmentBillingIapBinding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.userInteractionState
+            .onEach {
+                when (it) {
+                    is BillingViewModel.UserInteractionState.OnLoadingStateChange -> onLoading(it.loading)
+                    is BillingViewModel.UserInteractionState.OnPay -> onPay()
+                    is BillingViewModel.UserInteractionState.PlanValidated -> setPlan(it.plan)
+                    else -> {
+                        // no operation, not interested in other events
+                    }
+                }.exhaustive
+            }.launchIn(lifecycleScope)
+    }
+
+    private fun onLoading(loading: Boolean) {
+        // TODO
+    }
+
+    private fun onPay() {
+        // TODO
+    }
+
+    private fun setPlan(plan: PlanShortDetails) {
+        // the plan price should come from the Billing Library
+        binding.selectedPlanDetailsLayout.plan = plan
+    }
+}
---
+++ payment-iap/presentation/src/main/kotlin/me/proton/core/paymentiap/presentation/view/PlanShortDetailsView.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2020 Proton Technologies AG
+ * This file is part of Proton Technologies AG and ProtonCore.
+ *
+ * ProtonCore is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonCore is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonCore.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package me.proton.core.paymentiap.presentation.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import me.proton.core.paymentcommon.domain.entity.SubscriptionCycle
+import me.proton.core.paymentcommon.presentation.entity.PlanShortDetails
+import me.proton.core.paymentiap.presentation.R
+import me.proton.core.paymentiap.presentation.databinding.PlanShortDetailsBinding
+import me.proton.core.presentation.utils.formatCentsPriceDefaultLocale
+
+internal class PlanShortDetailsView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : ConstraintLayout(context, attrs, defStyleAttr) {
+
+    private val binding = PlanShortDetailsBinding.inflate(LayoutInflater.from(context), this)
+
+    init {
+        binding.amountProgress.visibility = View.VISIBLE
+    }
+
+    var userReadablePlanAmount: String? = null
+
+    var plan: PlanShortDetails? = null
+        set(value) = with(binding) {
+            val notAvailable = context.getString(R.string.payments_info_not_available)
+            planNameText.text = value?.displayName ?: notAvailable
+            billingPeriodText.text = when (value?.subscriptionCycle) {
+                SubscriptionCycle.YEARLY -> context.getString(R.string.payments_billing_yearly)
+                else -> notAvailable
+            }
+            value?.amount?.let {
+                amountProgress.visibility = View.GONE
+                userReadablePlanAmount = it.toDouble().formatCentsPriceDefaultLocale(value.currency.name)
+                amountText.text = userReadablePlanAmount
+            } ?: run {
+                amountProgress.visibility = View.VISIBLE
+            }
+        }
+}
