37680c7872f5ea12efbf80f0241b53dc2dd4bab7
Neil Marietta <neil.marietta@protonmail.com>
2022-08-29 18:17:30 +0200

    chore: Added Quark setPaymentMethods.

--- test/android/instrumented/src/main/kotlin/me/proton/core/test/android/plugins/Quark.kt
+++ test/android/instrumented/src/main/kotlin/me/proton/core/test/android/plugins/Quark.kt
@@ -21,12 +21,21 @@ package me.proton.core.test.android.plugins
 import android.util.Log
 import androidx.test.platform.app.InstrumentationRegistry
 import kotlinx.serialization.Serializable
+import me.proton.core.domain.entity.AppStore
 import me.proton.core.test.android.instrumented.ProtonTest.Companion.testTag
+import me.proton.core.test.android.plugins.Quark.InternalApiEndpoint.DRIVE_POPULATE_USER_WITH_DATA
+import me.proton.core.test.android.plugins.Quark.InternalApiEndpoint.JAIL_UNBAN
+import me.proton.core.test.android.plugins.Quark.InternalApiEndpoint.PAYMENTS_SEED_PAYMENT_METHOD
+import me.proton.core.test.android.plugins.Quark.InternalApiEndpoint.PAYMENTS_SEED_SUBSCRIBER
+import me.proton.core.test.android.plugins.Quark.InternalApiEndpoint.SYSTEM_ENV
+import me.proton.core.test.android.plugins.Quark.InternalApiEndpoint.USER_CREATE
 import me.proton.core.test.android.plugins.data.User
 import me.proton.core.test.android.plugins.data.randomPaidPlan
 import me.proton.core.util.kotlin.deserialize
+import me.proton.core.util.kotlin.toInt
 import okhttp3.OkHttpClient
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import java.util.concurrent.TimeUnit
 
 class Quark(private val host: String, private val proxyToken: String?, internalApiJsonPath: String) {
@@ -38,6 +47,7 @@ class Quark(private val host: String, private val proxyToken: String?, internalA
     )
 
     enum class InternalApiEndpoint {
+        SYSTEM_ENV,
         JAIL_UNBAN,
         USER_CREATE,
         PAYMENTS_SEED_PAYMENT_METHOD,
@@ -61,26 +71,42 @@ class Quark(private val host: String, private val proxyToken: String?, internalA
         .use { it.readText() }
         .deserialize()
 
-
     val defaultVerificationCode = internalApi.constants["DEFAULT_VERIFICATION_CODE"]!!
-    val planDevName: String = internalApi.constants["PLAN_DEV_NAME"]!!
-    val planDevText: String = internalApi.constants["PLAN_DEV_TEXT"]!!
 
-    private fun quarkRequest(endpoint: String, args: Array<String> = emptyArray()): String {
-        val argString = args.filter { it.isNotEmpty() }.joinToString("&")
-        val url = "https://${host}$endpoint?$argString"
-        val req = Request.Builder().url(url).apply { proxyToken?.let { header("x-atlas-secret", proxyToken) } }.build()
-
-        client.newCall(req).execute().use {
-            Log.d(testTag, "\nSent request to endpoint : $endpoint; Response Code: ${it.code}")
-            if (it.code > 201) {
-                throw Exception(it.body!!.string())
-            }
-            return it.body!!.string()
+    private fun executeRequest(request: Request): String {
+        client.newCall(request).execute().use {
+            Log.d(testTag, "\nSent request to endpoint : ${request.url}; Response Code: ${it.code}")
+            check(it.code <= 201) { "Response code error: ${it.body?.string()}" }
+            return requireNotNull(it.body).string()
         }
     }
 
-    fun jailUnban() = internalApi.endpoints[InternalApiEndpoint.JAIL_UNBAN]?.let { quarkRequest(it) }
+    private fun prepareRequest(endpoint: String, args: Array<String>): Request.Builder {
+        val argString = args.filter { it.isNotEmpty() }.joinToString("&")
+        val url = "https://${host}$endpoint?$argString"
+        return Request.Builder().url(url).apply {
+            proxyToken?.let<String, Unit> { header("x-atlas-secret", proxyToken) }
+        }
+    }
+
+    private fun quarkRequest(
+        endpoint: InternalApiEndpoint,
+        args: Array<String> = emptyArray(),
+        builder: Request.Builder.() -> Unit = {}
+    ): String? {
+        return internalApi.endpoints[endpoint]?.let {
+            val requestBuilder = prepareRequest(it, args)
+            builder.invoke(requestBuilder)
+            executeRequest(requestBuilder.build())
+        }
+    }
+
+    fun systemEnv(variable: String, value: String) {
+        val args = arrayOf("$variable=$value")
+        quarkRequest(SYSTEM_ENV, args) { post("".toRequestBody()) }
+    }
+
+    fun jailUnban() = quarkRequest(JAIL_UNBAN)
 
     fun seedSubscriber(user: User = User(plan = randomPaidPlan())): User {
         val args = arrayOf(
@@ -88,9 +114,7 @@ class Quark(private val host: String, private val proxyToken: String?, internalA
             "password=${user.password}",
             "plan=${user.plan.planName}"
         )
-        internalApi.endpoints[InternalApiEndpoint.PAYMENTS_SEED_SUBSCRIBER]?.let {
-            quarkRequest(it, args)
-        }
+        quarkRequest(PAYMENTS_SEED_SUBSCRIBER, args)
         return user
     }
 
@@ -100,9 +124,7 @@ class Quark(private val host: String, private val proxyToken: String?, internalA
             "-p=${user.password}",
             "-t=card"
         )
-        internalApi.endpoints[InternalApiEndpoint.PAYMENTS_SEED_PAYMENT_METHOD]?.let {
-            quarkRequest(it, args)
-        }
+        quarkRequest(PAYMENTS_SEED_PAYMENT_METHOD, args)
         return user
     }
 
@@ -114,9 +136,7 @@ class Quark(private val host: String, private val proxyToken: String?, internalA
             "-z=${internalApi.constants["PAYPAL_PASSWORD"]!!}",
             "-t=paypal"
         )
-        internalApi.endpoints[InternalApiEndpoint.PAYMENTS_SEED_PAYMENT_METHOD]?.let {
-            quarkRequest(it, args)
-        }
+        quarkRequest(PAYMENTS_SEED_PAYMENT_METHOD, args)
     }
 
     fun userCreate(
@@ -133,7 +153,7 @@ class Quark(private val host: String, private val proxyToken: String?, internalA
             "-k=$keysEncryption"
         )
 
-        quarkRequest(internalApi.endpoints[InternalApiEndpoint.USER_CREATE]!!, args)
+        quarkRequest(USER_CREATE, args)
         return user
     }
 
@@ -145,7 +165,23 @@ class Quark(private val host: String, private val proxyToken: String?, internalA
             if (user.password.isNotEmpty()) "-p=${user.password}" else "",
             if (user.dataSetScenario.isNotEmpty()) "-S=${user.dataSetScenario}" else "",
         )
-        quarkRequest(internalApi.endpoints[InternalApiEndpoint.DRIVE_POPULATE_USER_WITH_DATA]!!, args)
+        quarkRequest(DRIVE_POPULATE_USER_WITH_DATA, args)
         return user
+    }
+
+    fun setDefaultPaymentMethods() = setPaymentMethods(card = true, paypal = true, inApp = true)
+
+    fun setPaymentMethods(
+        appStore: AppStore = AppStore.GooglePlay,
+        card: Boolean = true,
+        paypal: Boolean = true,
+        inApp: Boolean = true
+    ) {
+        val value: Int = (card.toInt() shl 0) + (paypal.toInt() shl 1) + (inApp.toInt() shl 2)
+        val env = when (appStore) {
+            AppStore.FDroid -> internalApi.constants["ENV_PAYMENT_STATUS_STORE_FDROID"]
+            AppStore.GooglePlay -> internalApi.constants["ENV_PAYMENT_STATUS_STORE_GOOGLE"]
+        }
+        systemEnv(requireNotNull(env), "$value")
     }
 }
