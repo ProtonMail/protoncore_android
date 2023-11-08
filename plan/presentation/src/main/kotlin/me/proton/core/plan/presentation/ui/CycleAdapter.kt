26b549c703c2fca8c7d616251d1007b82794d01b
Mateusz Armatys <mateusz.armatys@proton.ch>
2023-11-08 16:06:47 +0100

    fix(plan): Change `plans_pay_other` string to plural.

--- plan/presentation/src/main/kotlin/me/proton/core/plan/presentation/ui/CycleAdapter.kt
+++ plan/presentation/src/main/kotlin/me/proton/core/plan/presentation/ui/CycleAdapter.kt
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2023 Proton AG
+ * This file is part of Proton AG and ProtonCore.
+ *
+ * ProtonCore is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonCore is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonCore.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package me.proton.core.plan.presentation.ui
 
 import android.content.Context
@@ -31,6 +49,6 @@ class CycleAdapter(
         1 -> context.getString(R.string.plans_pay_monthly)
         12 -> context.getString(R.string.plans_pay_annually)
         24 -> context.getString(R.string.plans_pay_biennially)
-        else -> context.getString(R.string.plans_pay_other, cycle)
+        else -> context.resources.getQuantityString(R.plurals.plans_pay_other, cycle, cycle)
     }
 }
