a4b8b2c5b2238d6e34a55dc157bbfae4be035c50
Neil Marietta <neil.marietta@protonmail.com>
2024-06-24 14:36:22 +0200

    test(auth): Added FakeIsCommonPasswordCheckEnabled.

---
+++ auth/test/src/main/kotlin/me/proton/core/auth/test/fake/FakeIsCommonPasswordCheckEnabled.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2024 Proton AG
+ *
+ * This file is part of ProtonVPN.
+ *
+ * ProtonVPN is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonVPN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package me.proton.core.auth.test.fake
+
+import me.proton.core.auth.domain.IsCommonPasswordCheckEnabled
+import me.proton.core.domain.entity.UserId
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+public class FakeIsCommonPasswordCheckEnabled(
+    private val enabled: Boolean = false
+) : IsCommonPasswordCheckEnabled {
+
+    @Inject
+    public constructor() : this(enabled = false)
+
+    override fun invoke(userId: UserId?): Boolean = enabled
+    override fun isLocalEnabled(): Boolean = enabled
+    override fun isRemoteEnabled(userId: UserId?): Boolean = enabled
+}
