47c42f292a952e92d345c62e431fad206b547b21
dkadrikj <kadrikj@protonmail.ch>
2024-09-19 13:21:03 +0200

    feat(auth): Add Request Access Granted/Denied compose.

---
+++ auth/presentation-compose/src/test/kotlin/me/proton/core/auth/presentation/compose/sso/RequestAccessResultScreenTest.kt
@@ -0,0 +1,33 @@
+package me.proton.core.auth.presentation.compose.sso
+
+import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.Paparazzi
+import me.proton.core.compose.theme.ProtonTheme
+import org.junit.Rule
+import kotlin.test.Test
+
+class RequestAccessResultScreenTest {
+    @get:Rule
+    val paparazzi = Paparazzi(
+        deviceConfig = DeviceConfig.PIXEL_5,
+        theme = "ProtonTheme"
+    )
+
+    @Test
+    fun `access granted screen`() {
+        paparazzi.snapshot {
+            ProtonTheme {
+                RequestAccessGrantedScreen()
+            }
+        }
+    }
+
+    @Test
+    fun `access denied screen`() {
+        paparazzi.snapshot {
+            ProtonTheme {
+                RequestAccessDeniedScreen()
+            }
+        }
+    }
+}
