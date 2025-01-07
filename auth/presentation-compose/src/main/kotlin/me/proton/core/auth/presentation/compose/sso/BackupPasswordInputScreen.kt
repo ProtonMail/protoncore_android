ad07de4cc9a4ac8e374ff3116e66f7c85f9031df
Neil Marietta <neil.marietta@protonmail.com>
2025-01-07 14:04:47 +0100

    feat(auth): Added BackupPasswordInputScreen default keyboardAction.

--- auth/presentation-compose/src/main/kotlin/me/proton/core/auth/presentation/compose/sso/BackupPasswordInputScreen.kt
+++ auth/presentation-compose/src/main/kotlin/me/proton/core/auth/presentation/compose/sso/BackupPasswordInputScreen.kt
@@ -24,6 +24,8 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -38,6 +40,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -156,6 +160,11 @@ public fun BackupPasswordInputScreen(
                     enabled = !isLoading,
                     singleLine = true,
                     label = { Text(text = stringResource(id = R.string.backup_password_input_label)) },
+                    keyboardOptions = KeyboardOptions(
+                        imeAction = ImeAction.Done,
+                        keyboardType = KeyboardType.Password
+                    ),
+                    keyboardActions = KeyboardActions { onPasswordSubmitted(Submit(password)) },
                     errorText = passwordError?.let { stringResource(it) },
                     modifier = Modifier.padding(top = ProtonDimens.MediumSpacing)
                 )
