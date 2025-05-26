78f2950afc4912702f698f67f0ec8a3d33102c9d
Mateusz Armatys <mateusz.armatys@proton.ch>
2025-05-26 17:28:11 +0200

    refactor(pass-validator): Extract style for PasswordPolicyReport view.

--- pass-validator/presentation/src/main/kotlin/me/proton/core/passvalidator/presentation/report/PasswordPolicyReport.kt
+++ pass-validator/presentation/src/main/kotlin/me/proton/core/passvalidator/presentation/report/PasswordPolicyReport.kt
@@ -20,8 +20,6 @@ package me.proton.core.passvalidator.presentation.report
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
@@ -32,13 +30,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.flow.Flow
+import me.proton.core.compose.theme.LocalColors
+import me.proton.core.compose.theme.LocalTypography
 import me.proton.core.compose.theme.ProtonDimens
 import me.proton.core.compose.theme.ProtonTheme
 import me.proton.core.compose.theme.captionHint
@@ -47,12 +49,32 @@ import me.proton.core.compose.viewmodel.hiltViewModelOrNull
 import me.proton.core.domain.entity.UserId
 import me.proton.core.passvalidator.presentation.R
 
+public data class PasswordPolicyReportStyle(
+    val brandNorm: Color,
+    val iconHint: Color,
+    val notificationError: Color,
+    val captionHint: TextStyle,
+    val captionRegular: TextStyle,
+    val captionWeak: TextStyle
+)
+
+@Composable
+public fun LegacyPasswordPolicyReportStyle(): PasswordPolicyReportStyle = PasswordPolicyReportStyle(
+    brandNorm = LocalColors.current.brandNorm,
+    iconHint = LocalColors.current.iconHint,
+    notificationError = LocalColors.current.notificationError,
+    captionHint = LocalTypography.current.captionHint,
+    captionRegular = LocalTypography.current.captionRegular,
+    captionWeak = LocalTypography.current.captionWeak,
+)
+
 @Composable
 public fun PasswordPolicyReport(
     passwordFlow: Flow<String>,
     userId: UserId?,
     onResult: (isPasswordValid: Boolean) -> Unit,
     modifier: Modifier = Modifier,
+    style: PasswordPolicyReportStyle = LegacyPasswordPolicyReportStyle(),
     viewModel: PasswordPolicyReportViewModel? = hiltViewModelOrNull(key = userId?.id)
 ) {
     val password by passwordFlow.collectAsStateWithLifecycle(initialValue = "")
@@ -62,6 +84,7 @@ public fun PasswordPolicyReport(
         userId = userId,
         onResult = onResult,
         modifier = modifier,
+        style = style,
         viewModel = viewModel
     )
 }
@@ -72,6 +95,7 @@ public fun PasswordPolicyReport(
     userId: UserId?,
     onResult: (isPasswordValid: Boolean) -> Unit,
     modifier: Modifier = Modifier,
+    style: PasswordPolicyReportStyle = LegacyPasswordPolicyReportStyle(),
     viewModel: PasswordPolicyReportViewModel? = hiltViewModelOrNull(key = userId?.id)
 ) {
     val state by viewModel?.state?.collectAsStateWithLifecycle() ?: return
@@ -83,7 +107,8 @@ public fun PasswordPolicyReport(
     PasswordPolicyReport(
         state = state,
         modifier = modifier,
-        onResult = onResult
+        onResult = onResult,
+        style = style
     )
 }
 
@@ -91,7 +116,8 @@ public fun PasswordPolicyReport(
 internal fun PasswordPolicyReport(
     state: PasswordPolicyReportState,
     modifier: Modifier = Modifier,
-    onResult: (isPasswordValid: Boolean) -> Unit = {}
+    onResult: (isPasswordValid: Boolean) -> Unit = {},
+    style: PasswordPolicyReportStyle = LegacyPasswordPolicyReportStyle()
 ) {
     LaunchedEffect(state) {
         onResult(state is PasswordPolicyReportState.Idle && state.allPassed())
@@ -100,14 +126,19 @@ internal fun PasswordPolicyReport(
     when (state) {
         is PasswordPolicyReportState.Loading -> Unit
         is PasswordPolicyReportState.Hidden -> Unit
-        is PasswordPolicyReportState.Idle -> PasswordPolicyReport(messages = state.messages, modifier = modifier)
+        is PasswordPolicyReportState.Idle -> PasswordPolicyReport(
+            messages = state.messages,
+            modifier = modifier,
+            style = style
+        )
     }
 }
 
 @Composable
-internal fun PasswordPolicyReport(
+private fun PasswordPolicyReport(
     messages: List<PasswordPolicyReportMessage>,
     modifier: Modifier = Modifier,
+    style: PasswordPolicyReportStyle = LegacyPasswordPolicyReportStyle()
 ) {
     val errors = remember(messages) { messages.filterIsInstance<PasswordPolicyReportMessage.Error>() }
     val hints = remember(messages) { messages.filterIsInstance<PasswordPolicyReportMessage.Hint>() }
@@ -117,13 +148,16 @@ internal fun PasswordPolicyReport(
         errors.forEach { msg ->
             PasswordPolicyError(
                 msg,
-                modifier = Modifier.padding(bottom = ProtonDimens.ExtraSmallSpacing)
+                modifier = Modifier.padding(bottom = ProtonDimens.ExtraSmallSpacing),
+                captionRegular = style.captionRegular,
+                notificationError = style.notificationError
             )
         }
 
         hints.forEach { hint ->
             PasswordPolicyHint(
                 hint,
+                captionWeak = style.captionWeak,
                 modifier = Modifier.padding(bottom = ProtonDimens.ExtraSmallSpacing)
             )
         }
@@ -131,14 +165,15 @@ internal fun PasswordPolicyReport(
         if (requirements.isNotEmpty()) {
             Text(
                 text = stringResource(R.string.password_policy_must_have_hint),
-                style = ProtonTheme.typography.captionWeak,
+                style = style.captionWeak,
                 modifier = Modifier.padding(bottom = ProtonDimens.ExtraSmallSpacing)
             )
         }
 
         requirements.forEach { msg ->
             PasswordPolicyRequirement(
-                msg,
+                msg = msg,
+                style = style,
                 modifier = Modifier.padding(bottom = ProtonDimens.ExtraSmallSpacing)
             )
         }
@@ -148,12 +183,14 @@ internal fun PasswordPolicyReport(
 @Composable
 private fun PasswordPolicyError(
     msg: PasswordPolicyReportMessage.Error,
-    modifier: Modifier = Modifier
+    captionRegular: TextStyle,
+    notificationError: Color,
+    modifier: Modifier = Modifier,
 ) {
     Text(
         text = msg.message,
-        style = ProtonTheme.typography.captionRegular,
-        color = ProtonTheme.colors.notificationError,
+        style = captionRegular,
+        color = notificationError,
         modifier = modifier
     )
 }
@@ -161,11 +198,12 @@ private fun PasswordPolicyError(
 @Composable
 private fun PasswordPolicyHint(
     msg: PasswordPolicyReportMessage.Hint,
+    captionWeak: TextStyle,
     modifier: Modifier = Modifier
 ) {
     Text(
         text = msg.message,
-        style = ProtonTheme.typography.captionWeak,
+        style = captionWeak,
         modifier = modifier
     )
 }
@@ -173,6 +211,7 @@ private fun PasswordPolicyHint(
 @Composable
 private fun PasswordPolicyRequirement(
     msg: PasswordPolicyReportMessage.Requirement,
+    style: PasswordPolicyReportStyle,
     modifier: Modifier = Modifier
 ) {
     Row(
@@ -189,15 +228,15 @@ private fun PasswordPolicyRequirement(
                 .padding(if (msg.success) 0.dp else 2.dp)
                 .align(Alignment.CenterVertically),
             tint = when {
-                msg.success -> ProtonTheme.colors.brandNorm
-                else -> ProtonTheme.colors.iconHint
+                msg.success -> style.brandNorm
+                else -> style.iconHint
             }
         )
         Text(
             text = msg.message,
             style = when {
-                msg.success -> ProtonTheme.typography.captionHint
-                else -> ProtonTheme.typography.captionWeak
+                msg.success -> style.captionHint
+                else -> style.captionWeak
             },
             textDecoration = when {
                 msg.success -> TextDecoration.LineThrough
